4850739e43cf0a57b5bc423079dc81153173ef13
JeddSJr <55459056+JeddSJr@users.noreply.github.com>
2020-11-04 07:59:22 -0500

    execise 1 to 4

--- EchoServer.java
+++ EchoServer.java
@@ -1,12 +1,13 @@
 // This file contains material supporting section 3.7 of the textbook:
 // "Object Oriented Software Engineering" and is issued under the open-source
-// license found at www.lloseng.com 
+// license found at www.lloseng.com
 
 import java.io.*;
 import ocsf.server.*;
+import common.*;
 
 /**
- * This class overrides some of the methods in the abstract 
+ * This class overrides some of the methods in the abstract
  * superclass in order to give more functionality to the server.
  *
  * @author Dr Timothy C. Lethbridge
@@ -15,30 +16,36 @@ import ocsf.server.*;
  * @author Paul Holden
  * @version July 2000
  */
-public class EchoServer extends AbstractServer 
+public class EchoServer extends AbstractServer
 {
   //Class variables *************************************************
-  
+
   /**
    * The default port to listen on.
    */
   final public static int DEFAULT_PORT = 5555;
-  
+  /**
+   * The interface type variable.  It allows the implementation of
+   * the display method in the server.
+   */
+   ChatIF serverUI;
+
   //Constructors ****************************************************
-  
+
   /**
    * Constructs an instance of the echo server.
    *
    * @param port The port number to connect on.
    */
-  public EchoServer(int port) 
+  public EchoServer(int port, ChatIF serverUI)
   {
     super(port);
+    this.serverUI=serverUI;
   }
 
-  
+
   //Instance methods ************************************************
-  
+
   /**
    * This method handles any messages received from the client.
    *
@@ -49,9 +56,83 @@ public class EchoServer extends AbstractServer
     (Object msg, ConnectionToClient client)
   {
     System.out.println("Message received: " + msg + " from " + client);
-    this.sendToAllClients(msg);
+    if(((String)msg).startsWith("#login ") && client.getInfo("id") == null){
+        int loginid = Integer.parseInt(((String)msg).replaceAll("#login ", ""));
+        client.setInfo("id", loginid);
+    }
+    else if (((String)msg).startsWith("#login ") && client.getInfo("id") != null) {
+      try {
+        client.close();
+      } catch(Exception e) {}
+      System.out.println("Error: deja login");
+    }
+    else if (((String)msg).equals("#quit")) {
+      try {
+        client.close();
+      } catch(Exception e) {  }
+      System.out.println("Disconnecting client: "+client.getInfo("id"));
+    }
+    else{
+      this.sendToAllClients(client.getInfo("id")+":"+msg);
+    }
   }
-    
+
+  /**
+   * This method handles all data coming from the UI
+   *
+   * @param message The message from the UI.
+   */
+  public void handleMessageFromServerUI(String message)
+  {
+    try
+    {
+      serverUI.display("SERVER MSG "+message);
+      sendToAllClients("SERVER MSG "+message);
+      if(message.startsWith("#")==true){
+        switch (message) {
+          case("#quit"):
+            sendToAllClients("SERVER MSG "+message);
+            System.exit(0);
+            break;
+          case("#stop"):
+            this.stopListening();;
+            break;
+          case("#close"):
+            this.close();
+            break;
+          case("#start"):
+            if(!this.isListening()){
+              this.listen();
+            }
+            else{
+              System.out.println("Le serveur est en marche");
+            }
+            break;
+          case("#getport"):
+            System.out.println(this.getPort());
+            break;
+          default:
+          if(message.startsWith("#setport ")){
+            if(!this.isListening()){
+              int port = Integer.parseInt(message.replaceAll("#setport ", ""));
+              this.setPort(port);
+            }
+            else{
+              System.out.println("Erreur: Vous devez vous deconnecte pour changer de port ou host.");
+            }
+          }
+        }
+      }
+    }
+    catch(IOException e)
+    {
+      System.out.println(e);
+      serverUI.display
+        ("Could not read UI's message.  Terminating UI.");
+      System.exit(0);
+    }
+  }
+
   /**
    * This method overrides the one in the superclass.  Called
    * when the server starts listening for connections.
@@ -61,7 +142,7 @@ public class EchoServer extends AbstractServer
     System.out.println
       ("Server listening for connections on port " + getPort());
   }
-  
+
   /**
    * This method overrides the one in the superclass.  Called
    * when the server stops listening for connections.
@@ -71,17 +152,22 @@ public class EchoServer extends AbstractServer
     System.out.println
       ("Server has stopped listening for connections.");
   }
-  
+
+  protected void clientConnected(ConnectionToClient client) {
+    System.out.println("Connection of: " +client.toString()+ "");
+  }
+
+
   //Class methods ***************************************************
-  
+
   /**
-   * This method is responsible for the creation of 
+   * This method is responsible for the creation of
    * the server instance (there is no UI in this phase).
    *
-   * @param args[0] The port number to listen on.  Defaults to 5555 
+   * @param args[0] The port number to listen on.  Defaults to 5555
    *          if no argument is entered.
    */
-  public static void main(String[] args) 
+  public static void main(String[] args)
   {
     int port = 0; //Port to listen on
 
@@ -93,14 +179,15 @@ public class EchoServer extends AbstractServer
     {
       port = DEFAULT_PORT; //Set port to 5555
     }
-	
-    EchoServer sv = new EchoServer(port);
-    
-    try 
+
+    ChatIF serverUI= new ServerConsole(port);
+    EchoServer sv = new EchoServer(port, serverUI);
+
+    try
     {
       sv.listen(); //Start listening for connections
-    } 
-    catch (Exception ex) 
+    }
+    catch (Exception ex)
     {
       System.out.println("ERROR - Could not listen for clients!");
     }
--- client/ChatClient.java
+++ client/ChatClient.java
@@ -1,13 +1,12 @@
 // This file contains material supporting section 3.7 of the textbook:
 // "Object Oriented Software Engineering" and is issued under the open-source
-// license found at www.lloseng.com 
+// license found at www.lloseng.com
 
 package client;
 
 import ocsf.client.*;
 import common.*;
 import java.io.*;
-
 /**
  * This class overrides some of the methods defined in the abstract
  * superclass in order to give more functionality to the client.
@@ -20,16 +19,18 @@ import java.io.*;
 public class ChatClient extends AbstractClient
 {
   //Instance variables **********************************************
-  
+
   /**
-   * The interface type variable.  It allows the implementation of 
+   * The interface type variable.  It allows the implementation of
    * the display method in the client.
    */
-  ChatIF clientUI; 
-
-  
+  ChatIF clientUI;
+  /**
+   * The id of the client
+   */
+  int idLogin;
   //Constructors ****************************************************
-  
+
   /**
    * Constructs an instance of the chat client.
    *
@@ -37,38 +38,98 @@ public class ChatClient extends AbstractClient
    * @param port The port number to connect on.
    * @param clientUI The interface type variable.
    */
-  
-  public ChatClient(String host, int port, ChatIF clientUI) 
-    throws IOException 
+
+  public ChatClient(int idLogin,String host, int port, ChatIF clientUI)
+    throws IOException
   {
     super(host, port); //Call the superclass constructor
     this.clientUI = clientUI;
-    openConnection();
+    this.idLogin=idLogin;
+    try{
+      openConnection();
+      sendToServer("#login "+idLogin);
+    }
+    catch (Exception e) {
+      clientUI.display
+        ("Could not login to server.  Terminating client.");
+      quit();
+    }
   }
 
-  
+
   //Instance methods ************************************************
-    
+
   /**
    * This method handles all data that comes in from the server.
    *
    * @param msg The message from the server.
    */
-  public void handleMessageFromServer(Object msg) 
+  public void handleMessageFromServer(Object msg)
   {
     clientUI.display(msg.toString());
+    if(((String)msg).equals("SERVER MSG #quit")){
+      connectionClosed();
+    }
   }
 
   /**
-   * This method handles all data coming from the UI            
+   * This method handles all data coming from the UI
    *
-   * @param message The message from the UI.    
+   * @param message The message from the UI.
    */
   public void handleMessageFromClientUI(String message)
   {
     try
     {
-      sendToServer(message);
+      if(message.startsWith("#")==true){
+        switch (message) {
+          case("#quit"):
+            sendToServer("#quit");
+            this.quit();
+            break;
+          case("#logoff"):
+            this.closeConnection();
+            break;
+          case("#login"):
+            if(this.isConnected()==false){
+              sendToServer("#login "+idLogin);
+            }
+            else{
+              System.out.println("Erreur: Vous etes deja connecte.");
+            }
+            break;
+          case("#gethost"):
+            System.out.println(this.getHost());
+            break;
+          case("#getport"):
+            System.out.println(this.getPort());
+            break;
+          default:
+            if(message.startsWith("#set")){
+              if(this.isConnected()==false){
+                if (message.startsWith("#sethost ")) {
+                  String host = message.replaceAll("#sethost ", "");
+                  this.setHost(host);
+                }
+                else if (message.startsWith("#setport ")){
+                  int port = Integer.parseInt(message.replaceAll("#setport ", ""));
+                  this.setPort(port);
+                }
+              }
+            else{
+              System.out.println("Erreur: Vous devez vous deconnecte pour changer de port ou host.");
+            }
+            }
+            else{
+              sendToServer(message);
+            }
+          break;
+        }
+      }
+      else{
+        openConnection();
+        sendToServer(message);
+      }
     }
     catch(IOException e)
     {
@@ -77,7 +138,7 @@ public class ChatClient extends AbstractClient
       quit();
     }
   }
-  
+
   /**
    * This method terminates the client.
    */
@@ -89,6 +150,17 @@ public class ChatClient extends AbstractClient
     }
     catch(IOException e) {}
     System.exit(0);
+  }
+
+  public void connectionClosed(){
+    if(!this.isConnected()){
+      System.out.println("Disconnecting from server.");
+      System.exit(0);
+    }
+  }
+
+  protected void connectionException(Exception exception){
+
   }
 }
 //End of ChatClient class
